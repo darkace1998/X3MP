3471de616eb80564301b9321c082d5089174f1cd
DARKACE <32885015+darkace1998@users.noreply.github.com>
2025-07-21 10:48:36 +0200

    Delete Server directory

--- Server/Universe.cpp
+++
@@ -1,7 +0,0 @@
-#include "Universe.h"
-
-Universe::Universe()
-{
-    entities = std::make_shared<std::array<std::shared_ptr<x3::net::Entity>, 65535>>();
-    stars = std::make_shared<std::array<std::shared_ptr<x3::net::Entity>, 65535>>();
-}
